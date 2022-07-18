538a36bdc85ef6ccdb60eee0d3df957f855eec48
matt <matt@multicode.co.uk>
2022-07-18 11:46:59 +0000

    new trans form

--- src/NewTransactions/NewTransaction.js
+++ src/NewTransactions/NewTransaction.js
@@ -1,5 +1,53 @@
+import { useReducer } from "react";
+
 const NewTransaction = () => {
-    return <p>You are on the new transaction page</p>
+
+
+    const emptyTransaction = { orderId: "", date : new Date().toISOString().slice(0,10) , country: "",
+        amount : "", currency: "", taxCode : "", taxRate : "", type : ""}
+
+    const newTransactionReducer = (state, data) => {
+        return {...state, [data.field] : data.value}
+    }
+
+    const [newTransaction, dispatch] = useReducer(newTransactionReducer , emptyTransaction);
+
+    const handleChange = (event) => {
+        const dataToChange = { field : event.target.id, value : event.target.value };
+        dispatch(dataToChange);
+    }
+    
+    const {orderId, date, country, amount, currency, taxCode, taxRate, type} = newTransaction;
+
+    return (
+    <form className="addTransactionsForm">
+    <h2>New transaction</h2>
+    <label htmlFor="orderId">Order Id</label>
+    <input type="text" id="orderId" onChange={handleChange} value={orderId} />
+    <br/>
+    <label htmlFor="date">Date</label>
+    <input type="date" id="date" onChange={handleChange} value={date}/>
+    <br/>
+    <label htmlFor="country">Country</label>
+    <input type="text"  id="country" onChange={handleChange} value={country} />
+    <br/>
+    <label htmlFor="currency">Currency</label>
+    <input type="text"  id="currency" onChange={handleChange} value = {currency} />
+    <br/>
+    <label htmlFor="amount">Amount</label>
+    <input type="text"  id="amount" onChange={handleChange} value={amount} />
+    <br/>
+    <label htmlFor="taxCode">Tax Code</label>
+    <input type="text"  id="taxCode" onChange={handleChange} value ={taxCode} />
+    <br/>
+    <label htmlFor="amount">Tax Rate</label>
+    <input type="text"  id="taxRate" onChange={handleChange} value={taxRate} />
+    <br/>
+    <label htmlFor="type">Type</label>
+    <input type="text"  id="type" onChange={handleChange} value={type}  />
+    <br/>
+    <button type="submit">Save</button>
+</form>)
 }
 
 export default NewTransaction;
