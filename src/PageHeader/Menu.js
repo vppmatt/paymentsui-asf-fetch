b0a6d4304f0de5b04f7e5edff3a24a37fa414ab7
matt <matt@multicode.co.uk>
2022-07-18 13:52:23 +0000

    links

--- src/PageHeader/Menu.js
+++ src/PageHeader/Menu.js
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 const Menu = (props) => {
     return <ul className="nav">
-        <li style={{cursor: "pointer"}}   onClick={ () => props.setSelectedPage("find")} >Find a transaction</li>
-        <li style={{cursor: "pointer"}} onClick={ () => props.setSelectedPage("new")} >New Transaction</li>
+        <li style={{cursor: "pointer"}} ><Link to="/find"> Find a transaction</Link></li>
+        <li style={{cursor: "pointer"}} ><Link to="/new">New Transaction</Link></li>
     </ul>
 }
 
