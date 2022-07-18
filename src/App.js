b37ef49720b20b90b69ef5eb622218aa2560553e
matt <matt@multicode.co.uk>
2022-07-18 13:59:44 +0000

    404 and home

--- src/App.js
+++ src/App.js
@@ -7,20 +7,26 @@ import FindATransaction from './Transactions/FindATransaction';
 import NewTransaction from './NewTransactions/NewTransaction';
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PageNotFound from './PageNotFound';
 
 function App() {
-
 
   return (
     <BrowserRouter>
     <div className="App">
+      
       <PageHeader/>
+    
         <Routes>
+        
           <Route path="/find" element = {<FindATransaction />} />
           <Route path="/new" element={<NewTransaction />} />
+          <Route path="/" element={<h1>Welcome to the application</h1>} />
+          <Route path="*" element={<PageNotFound />} />  
         </Routes>
+        
       </div>
-    </BrowserRouter>
+      </BrowserRouter>
   );
 }
 
