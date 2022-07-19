e86110c433f80448baf8de60bdc4ffe0d3043f2e
matt <matt@multicode.co.uk>
2022-07-19 13:00:46 +0000

    use selector and use dipatch

--- src/PageHeader/PageHeader.js
+++ src/PageHeader/PageHeader.js
@@ -1,12 +1,20 @@
+import { useDispatch, useSelector } from 'react-redux';
 import Menu from './Menu';
 import './PageHeader.css'
 
 const PageHeader = (props) => {
 
+    const username = useSelector( state => state.userName) ;
+
+    const dispatch = useDispatch();
+
+    const changeName = () => dispatch({ type : "update-username", value : "Sally"});
+
     return(
         <div className="pageHeader">
         <h1>Payments Application</h1>
         <Menu setSelectedPage={props.setSelectedPage} />
+        <p>Current user : {username}  <button onClick={changeName}  >change user</button> </p>
         </div>
     )
 
--- src/store/store.js
+++ src/store/store.js
@@ -5,12 +5,15 @@ const initialState = { countries : [] , lastFetch : null , userName:"Matt"};
 const paymentsReducer = (state = initialState, action) => {
     // {type : "clear-down"}   - remove all data back to initial state
     // {type : "refresh-countries", value : []} - update the countris + set the value of last fetch
-
+    // {type : "update-username", value : "Mike"}
     if (action.type === "clear-down") {
         return initialState;
     }
     else if (action.type === "refresh-countries") {
-        return {...state, countries : action.value.countries , lastFetch : new Date()}
+        return {...state, countries : action.value , lastFetch : new Date()}
+    }
+    else if (action.type === "update-username") {
+        return {...state, userName : action.value}
     }
     else {
         console.log("unknown redux action " + action.type);
