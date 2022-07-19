55d60c497cbb5435ace6d0f1d7f23776c76f1d14
matt <matt@multicode.co.uk>
2022-07-19 09:57:40 +0000

    typing in a test

--- src/Search/Search.test.js
+++ src/Search/Search.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react"
 import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+
 import Search from "./Search";
-
-
 
 test( "ensure warning message is not displayed when component first rendered",
     () => {
@@ -15,5 +15,20 @@ test( "ensure warning message is not displayed when component first rendered",
             const messageParagraph = screen.queryByText("Please enter a valid order id");
             //we expect the message isn't present
             expect(messageParagraph).not.toBeInTheDocument();
+    }
+)
+
+test( "ensure warning message is displayed when the user has only typed in a space",
+    () => {
+
+        //GIVEN
+            render(<BrowserRouter><Search /></BrowserRouter>);
+        //WHEN
+            const orderIdInput = screen.getByLabelText("Order Id:");
+            userEvent.type(orderIdInput, " ");
+        //THEN
+            const messageParagraph = screen.queryByText("Please enter a valid order id");
+            //we expect the message is present
+            expect(messageParagraph).toBeInTheDocument();
     }
 )
